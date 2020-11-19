b84e26cb4617b9676c2ac5f10ee8c7e2057e609d
Juan Manuel <silvarojasj@gmail.com>
2020-11-19 15:33:31 -0500

    nevo archivo

---
+++ kata.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+      id: 1,
+      name: "Ada",
+      username: "ada@ada-school.org",
+      rol: "MENTOR",
+    },
+    {
+      id: 2,
+      name: "Mentor",
+      username: "mentor@ada-school.org",
+      rol: "MENTOR",
+    },
+    {
+      id: 3,
+      name: "Mentor 2",
+      username: "mentor2@ada-school.org",
+      rol: "MENTOR",
+    },
+  ];
+  
+
